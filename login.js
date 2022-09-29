18aa79ff35a5744334a4b03a6c97394b9613c4b7
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2022-09-29 12:48:01 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+//escreva o nome da função. Essa função é chamada quando o botão 'login' é clicado
+function ???() {
+    //escreva o nome da variável que guardará o nome dos jogadores
+   ??? = document.getElementById("???").value;
+    
+   //escreva essa variável para que ela seja mostrada no localStorage do navegador
+      localStorage.setItem("player1Name", ???);
+      localStorage.setItem("player2Name", ???);
+  //escreva o nome da tela que você deseja mostrar quando o botão for clicado
+      window.location = "???";
+  }
+  
---
+++ page.js
@@ -0,0 +1,11 @@
+// Crie um variável que guardará a resposta
+var ???
+function send(){
+    // Digite o 'id' da caixa de texto, onde você pegará o número
+    number1 = document.getElementById("???").value;
+    number2 = document.getElementById("???").value;
+    //Digite o nome da variável que guardará a resposta
+   ???= parseInt(number1)*parseInt(number2);
+    document.getElementById("resu").innerHTML = "O resultado é " + ???;
+}
+  
